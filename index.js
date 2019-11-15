a64cebe6ba8286494a5446c00812fec44bac174a
Umer Munir <Umer.Munir@Teradata.com>
2019-11-15 11:45:21 +0100

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Umer again!');
+  res.send('Hello World from Master branch!');
 });
 
 var server = app.listen(3000, function () {
