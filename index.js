15818dd9ac0768e2201d8b0d155c62c2b3b54ef9
Umer Munir <Umer.Munir@Teradata.com>
2019-11-15 13:31:02 +0100

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Master branch!');
+  res.send('Hello World from master branch!');
 });
 
 var server = app.listen(3000, function () {
